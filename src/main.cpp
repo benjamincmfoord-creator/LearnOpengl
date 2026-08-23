c892072f741535447b816ac4bd16f988119d5b25
Ben Foord <benjamincmfoord@gmail.com>
2026-08-23 11:39:01 +0100

    model view projection

--- src/main.cpp
+++ src/main.cpp
@@ -24,25 +24,68 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-float vertices[] = {
-    // positions          // colors           // texture coords
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-};
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
 
 
 float texCoords[] = {
     0.0f, 0.0f,  // lower-left corner  
     1.0f, 0.0f,  // lower-right corner
     0.5f, 1.0f   // top-center corner
-};
-
-
-unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 2,
-    0, 2, 3  // first Triangle
 };
 
 int  success;
@@ -75,6 +118,8 @@ int main()
         return -1;
     } 
 
+    glEnable(GL_DEPTH_TEST);
+
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
 
@@ -91,51 +136,32 @@ int main()
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
 
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO); 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    //postion
-    // position attribute
-
+    // Position
     glVertexAttribPointer(
         0,
         3,
         GL_FLOAT,
         GL_FALSE,
-        8 * sizeof(float),
+        5 * sizeof(float),
         (void*)0
     );
     glEnableVertexAttribArray(0);
-    
-    // Color
-    glVertexAttribPointer(
-        1,
-        3,
-        GL_FLOAT,
-        GL_FALSE,
-        8 * sizeof(float),
-        (void*)(3 * sizeof(float))
-    );
-    glEnableVertexAttribArray(1);
 
     // Texture coordinates
     glVertexAttribPointer(
-        2,
+        1,
         2,
         GL_FLOAT,
         GL_FALSE,
-        8 * sizeof(float),
-        (void*)(6 * sizeof(float))
+        5 * sizeof(float),
+        (void*)(3 * sizeof(float))
     );
-    glEnableVertexAttribArray(2);
+    glEnableVertexAttribArray(1);
     
     // load and create a texture 
     // -------------------------
@@ -213,7 +239,7 @@ int main()
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
         // bind textures on corresponding texture units
         glActiveTexture(GL_TEXTURE0);
@@ -221,19 +247,42 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-
-        // create transformations
-        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-
+        
         // render container
         ourShader.use();
-        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
+        // create transformations
+        glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        glm::mat4 view          = glm::mat4(1.0f);
+        glm::mat4 projection    = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
+        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        // retrieve the matrix uniform locations
+        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view");
+        // pass them to the shaders (3 different ways)
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        ourShader.setMat4("projection", projection);
+
+        
+
+        // render container
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        for(unsigned int i = 0; i < 10; i++)
+            {
+                glm::mat4 model = glm::mat4(1.0f);
+                model = glm::translate(model, cubePositions[i]);
+                float angle = 20.0f * i; 
+                model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+                ourShader.setMat4("model", model);
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+
+        
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -243,7 +292,6 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
 
 
     glfwTerminate();
--- src/shader.cpp
+++ src/shader.cpp
@@ -20,22 +20,18 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 
     try
     {
-        // Open shader files
         vShaderFile.open(vertexPath);
         fShaderFile.open(fragmentPath);
 
-        // Read files
         std::stringstream vShaderStream;
         std::stringstream fShaderStream;
 
         vShaderStream << vShaderFile.rdbuf();
         fShaderStream << fShaderFile.rdbuf();
 
-        // Close files
         vShaderFile.close();
         fShaderFile.close();
 
-        // Convert streams to strings
         vertexCode = vShaderStream.str();
         fragmentCode = fShaderStream.str();
     }
@@ -47,11 +43,10 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
             << std::endl;
     }
 
-
-    // Now compile the shaders
     const char* vShaderCode = vertexCode.c_str();
     const char* fShaderCode = fragmentCode.c_str();
 
+    // Vertex shader
     unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
 
     glShaderSource(vertex, 1, &vShaderCode, NULL);
@@ -59,7 +54,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 
     checkCompileErrors(vertex, "VERTEX");
 
-
+    // Fragment shader
     unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
 
     glShaderSource(fragment, 1, &fShaderCode, NULL);
@@ -67,8 +62,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 
     checkCompileErrors(fragment, "FRAGMENT");
 
-
-    // Create shader program
+    // Shader program
     ID = glCreateProgram();
 
     glAttachShader(ID, vertex);
@@ -78,19 +72,18 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 
     checkCompileErrors(ID, "PROGRAM");
 
-
     glDeleteShader(vertex);
     glDeleteShader(fragment);
 }
 
 
-void Shader::use()
+void Shader::use() const
 {
     glUseProgram(ID);
 }
 
 
-void Shader::setBool(const std::string &name, bool value) const
+void Shader::setBool(const std::string& name, bool value) const
 {
     glUniform1i(
         glGetUniformLocation(ID, name.c_str()),
@@ -99,7 +92,7 @@ void Shader::setBool(const std::string &name, bool value) const
 }
 
 
-void Shader::setInt(const std::string &name, int value) const
+void Shader::setInt(const std::string& name, int value) const
 {
     glUniform1i(
         glGetUniformLocation(ID, name.c_str()),
@@ -108,7 +101,7 @@ void Shader::setInt(const std::string &name, int value) const
 }
 
 
-void Shader::setFloat(const std::string &name, float value) const
+void Shader::setFloat(const std::string& name, float value) const
 {
     glUniform1f(
         glGetUniformLocation(ID, name.c_str()),
@@ -117,14 +110,178 @@ void Shader::setFloat(const std::string &name, float value) const
 }
 
 
-void Shader::checkCompileErrors(unsigned int shader, std::string type)
+// ====================
+// Vec2
+// ====================
+
+void Shader::setVec2(
+    const std::string& name,
+    const glm::vec2& value
+) const
+{
+    glUniform2fv(
+        glGetUniformLocation(ID, name.c_str()),
+        1,
+        &value[0]
+    );
+}
+
+
+void Shader::setVec2(
+    const std::string& name,
+    float x,
+    float y
+) const
+{
+    glUniform2f(
+        glGetUniformLocation(ID, name.c_str()),
+        x,
+        y
+    );
+}
+
+
+// ====================
+// Vec3
+// ====================
+
+void Shader::setVec3(
+    const std::string& name,
+    const glm::vec3& value
+) const
+{
+    glUniform3fv(
+        glGetUniformLocation(ID, name.c_str()),
+        1,
+        &value[0]
+    );
+}
+
+
+void Shader::setVec3(
+    const std::string& name,
+    float x,
+    float y,
+    float z
+) const
+{
+    glUniform3f(
+        glGetUniformLocation(ID, name.c_str()),
+        x,
+        y,
+        z
+    );
+}
+
+
+// ====================
+// Vec4
+// ====================
+
+void Shader::setVec4(
+    const std::string& name,
+    const glm::vec4& value
+) const
+{
+    glUniform4fv(
+        glGetUniformLocation(ID, name.c_str()),
+        1,
+        &value[0]
+    );
+}
+
+
+void Shader::setVec4(
+    const std::string& name,
+    float x,
+    float y,
+    float z,
+    float w
+) const
+{
+    glUniform4f(
+        glGetUniformLocation(ID, name.c_str()),
+        x,
+        y,
+        z,
+        w
+    );
+}
+
+
+// ====================
+// Mat2
+// ====================
+
+void Shader::setMat2(
+    const std::string& name,
+    const glm::mat2& mat
+) const
+{
+    glUniformMatrix2fv(
+        glGetUniformLocation(ID, name.c_str()),
+        1,
+        GL_FALSE,
+        &mat[0][0]
+    );
+}
+
+
+// ====================
+// Mat3
+// ====================
+
+void Shader::setMat3(
+    const std::string& name,
+    const glm::mat3& mat
+) const
+{
+    glUniformMatrix3fv(
+        glGetUniformLocation(ID, name.c_str()),
+        1,
+        GL_FALSE,
+        &mat[0][0]
+    );
+}
+
+
+// ====================
+// Mat4
+// ====================
+
+void Shader::setMat4(
+    const std::string& name,
+    const glm::mat4& mat
+) const
+{
+    glUniformMatrix4fv(
+        glGetUniformLocation(ID, name.c_str()),
+        1,
+        GL_FALSE,
+        &mat[0][0]
+    );
+}
+
+
+// ====================
+// Error checking
+// ====================
+
+void Shader::checkCompileErrors(
+    unsigned int shader,
+    std::string type
+)
 {
     int success;
     char infoLog[1024];
 
     if (type != "PROGRAM")
     {
-        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        glGetShaderiv(
+            shader,
+            GL_COMPILE_STATUS,
+            &success
+        );
 
         if (!success)
         {
@@ -146,7 +303,11 @@ void Shader::checkCompileErrors(unsigned int shader, std::string type)
     }
     else
     {
-        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        glGetProgramiv(
+            shader,
+            GL_LINK_STATUS,
+            &success
+        );
 
         if (!success)
         {
